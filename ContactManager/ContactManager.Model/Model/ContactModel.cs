6d45468da744a0e12ea2600360bb026d375fba00
Milan-Ing <milannis_95@windowslive.com>
2017-07-19 13:54:44 +0200

    Dodat EF model

---
+++ ContactManager/ContactManager.Model/Model/ContactModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactManager.Data;
+
+namespace ContactManager.Model.Model
+{
+    public class ContactModel
+    {
+        public int ContactID { get; set; }
+        public int ContactTypeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public System.DateTime InsertDate { get; set; }
+    }
+}
---
+++ ContactManager/ContactManager.Model/Model/ContactTypeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Model.Model
+{
+    public class ContactTypeModel
+    {
+        public int TypeID { get; set; }
+        public string Caption { get; set; }
+    }
+}
